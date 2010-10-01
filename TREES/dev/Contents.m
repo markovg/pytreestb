0998450b0ece221f36913f3d2b51a267633e16b7
emuller <devnull@localhost>
2010-10-01 17:58:07 +0200

    Started dev area

---
+++ TREES/dev/Contents.m
@@ -0,0 +1,11 @@
+% DEVELOPER DOMAIN
+%
+% This package contains code which is not for user consumption
+% Use at own risk!
+%
+% Files
+%   resample2_tree - A sandbox for playing with the resampler.
+%
+% the TREES toolbox: edit, visualize and analyze neuronal trees
+% Copyright (C) 2009  Hermann Cuntz
+
